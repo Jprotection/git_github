91625643d684c05ccdf3f7a791a2ed8026b6fa6f
Jprotection <bigho98@gmail.com>
2025-05-07 09:06:28 +0900

    add와 commit 동시에

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -6,3 +6,15 @@ git commit -m "메시지"
 
 # 원격 repo main브랜치에 변경사항(commitID)을 업로드
 git push origin main
+
+# 현재 working directory, staging area 상태 조회
+git status
+
+# 특정 파일만 add할 경우
+git add 특정파일(위치포함)
+
+# local repository에 커밋 이력 생성
+git commit -m "메시지타이틀" -m "메시지내용"
+
+# vi에디터 모드에서 작성 : 첫줄에는 제목, 두번째 줄부터 내용
+git commit
